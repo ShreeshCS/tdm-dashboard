7e5158e80495e23f9be0ca00cef6d150c8dc7c77
Lahauriya, Shreesh <slahauriya@deloitte.com>
2023-03-29 13:00:24 +0530

    created project structure

---
+++ src/Components/Header/Childs/Logo.js
@@ -0,0 +1,10 @@
+//TODO - create and import Logo.module.css
+
+export default Logo = () => {
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/1/15/Deloitte_Logo.png"
+      alt="Deloitte. logo"
+    ></img>
+  );
+};
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import Logo from "./Childs/Logo";
+
+const Header = () => {
+    return(
+        <Logo />
+        <Heading />
+        <DasboardHeading />
+    );
+};
+
+export default Header;
